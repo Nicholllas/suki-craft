cb8141c3ebb2db9d8d58fa63de4032c312a32a68
Nicholllas <nicholaschristhofersinaga@gmail.com>
2026-08-18 01:13:36 +0700

    Perbaikan banner promo

--- tests/Feature/PromotionTest.php
+++ tests/Feature/PromotionTest.php
@@ -4,8 +4,13 @@ use App\Models\PromoBanner;
 
 test('storefront only receives active banners in display order', function () {
     $hiddenBanner = PromoBanner::factory()->create(['is_active' => false, 'sort_order' => 0]);
-    $secondBanner = PromoBanner::factory()->create(['sort_order' => 2, 'title' => 'Banner kedua']);
-    $firstBanner = PromoBanner::factory()->create(['sort_order' => 1, 'title' => 'Banner pertama']);
+    $secondBanner = PromoBanner::factory()->create(['image_path' => 'promo-banners/banner-kedua.jpg', 'sort_order' => 2, 'title' => 'Banner kedua']);
+    $firstBanner = PromoBanner::factory()->create(['image_path' => 'promo-banners/banner-pertama.jpg', 'sort_order' => 1, 'title' => 'Banner pertama']);
 
-    $this->get(route('home'))->assertOk()->assertSeeInOrder([$firstBanner->title, $secondBanner->title])->assertDontSee($hiddenBanner->title);
+    $this->get(route('home'))
+        ->assertOk()
+        ->assertSeeInOrder([$firstBanner->title, $secondBanner->title])
+        ->assertDontSee($hiddenBanner->title)
+        ->assertSee('/storage/promo-banners/banner-pertama.jpg', false)
+        ->assertSee('object-contain', false);
 });
